414b17d7603d6d638139fc4757a8a3b1fef796dc
Damian <HybrIT.Damian.Graczyk@outlook.com>
2022-06-04 13:36:03 +0200

    update #3 day_3

--- sda-app/src/app/views/students/students.component.ts
+++ sda-app/src/app/views/students/students.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Student } from './model/student';
 import { StudentsService } from './students.service';
@@ -10,15 +11,39 @@ import { StudentsService } from './students.service';
 })
 export class StudentsComponent implements OnInit {
   studentsList: Student[] = [];
+  studentForm: FormGroup = new FormGroup({});
 
-  constructor(private studentsService: StudentsService) { }
+  constructor(private studentsService: StudentsService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.getStudentsList();
+    this.createStudentForm();
   }
 
   private getStudentsList(): void {
     this.studentsService.getStudents().subscribe(studentsListAPI => this.studentsList = studentsListAPI);
+  }
+
+  createStudentForm(): void {
+    this.studentForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      dateOfBirth: ['1990-01-01', Validators.required],
+      fieldOfStudy: ['', Validators.required],
+      average: ['', Validators.required],
+    });
+  }
+
+  addStudent(): void {
+    this.studentsService.postStudent(this.studentForm.value).subscribe(() => {
+      this.getStudentsList();
+      this.createStudentForm();
+    });
+  }
+
+  removeStudent(id: number, ev: Event): void {
+    ev.stopPropagation();
+    this.studentsService.deleteStudent(id).subscribe(() => this.getStudentsList());
   }
 
 }
